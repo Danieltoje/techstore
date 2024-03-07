ef3c3624a7ac7c47218202b4d8b4128456141be7
daniel <danieltoje98@gmail.com>
2024-03-07 02:23:21 -0800

    model setup

---
+++ models/Order.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema 
+
+const orderSchema = new Schema({
+    userId: {
+        type: String,
+    },
+    products: [
+        {
+            productId: {
+                type: String,
+            },
+            quantity: {
+                type: Number
+                default: 1
+            }
+        },
+    ],
+    amount: {type: Number}
+    Address: {type: Object}
+    status: {type: String, default: "pending"}
+}, { timestamps: true})
+
+const orderModel = mongoose.model('Order', orderSchema)
+module.exports = orderModel
